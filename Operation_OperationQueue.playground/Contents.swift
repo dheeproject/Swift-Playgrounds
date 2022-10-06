c67ac42d4a4439427c98d79e16a8f57e8f6e910e
Dheeraj Mansukhani <62594025+dheeproject@users.noreply.github.com>
2022-10-06 22:33:42 +0530

    Added operation queue example
    Added operation queue example

---
+++ Operation_OperationQueue.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+struct Example
+{
+    func doWork() {
+
+        let blockOperation = BlockOperation()
+//        blockOperation.qualityOfService = .utility
+        blockOperation.addExecutionBlock {
+            debugPrint("Hello")
+        }
+
+        blockOperation.addExecutionBlock {
+            debugPrint("my name is")
+        }
+
+        blockOperation.addExecutionBlock {
+            debugPrint("Dheeraj")
+        }
+
+//        blockOperation.start()
+
+        let anotherBlockOperation = BlockOperation()
+        anotherBlockOperation.addExecutionBlock {
+            debugPrint("I am another block operation")
+        }
+
+        let operationQueue = OperationQueue()
+        operationQueue.qualityOfService = .utility
+//        //operationQueue.addOperation(blockOperation)
+//       // operationQueue.addOperation(anotherBlockOperation)
+//
+        operationQueue.addOperations([blockOperation, anotherBlockOperation], waitUntilFinished: false)
+
+    }
+}
+
+let obj = Example()
+obj.doWork()
